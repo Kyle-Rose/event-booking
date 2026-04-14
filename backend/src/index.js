6cd3ac602b71bb070d1b666374175ff7c8fc40a4
Kyle-Rose <kyle.rose1@snhu.edu>
2026-04-14 13:50:51 -0400

    debug backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,30 +1,38 @@
-const express = require("express");
-const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 3000;
+
+const express = require("express");
+const path = require("path");
 const cors = require("cors");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+const isProduction = process.env.NODE_ENV === "production";
+
+
 app.use(cors({
-  origin: "*"
+  origin: true,
+  credentials: true
 }));
+
+app.use(express.json());
+
 
 const authRoutes = require("./routes/authRoutes");
 const eventRoutes = require("./routes/eventRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
 
-app.use(express.json());
+
 app.use("/auth", authRoutes);
 app.use("/events", eventRoutes);
-app.use("/", bookingRoutes);
+app.use("/bookings", bookingRoutes);
 
-const path = require("path");
+app.use(express.static(path.join(__dirname, "..", "frontend")));
 
-// serve entire frontend folder
-app.use(express.static(path.join(__dirname, "../../frontend")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend/login/index.html"));
+  res.sendFile(path.join(__dirname, "..", "frontend", "login", "index.html"));x
 });
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
